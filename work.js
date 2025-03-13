32b264575f26b61bdd67a856cbe5e1a0c2f32673
Faizanhaider461 <syedfaizan768768@gmail.com>
2025-03-14 04:57:42 +0500

    Add files via upload

--- work.js
+++ work.js
@@ -29,5 +29,5 @@ function stopSnow() {
 
 
 function instructions(){
-    let c= alert("To Start Rain: Press (Start Rain) \nTo Stop Rain: Press (Stop Rain) \nTo Start Snow: Press (Start Snow) \nTo Start Rain + Snow: Press (Start Rain + Start Snow) \nTo Stop Rain + Snow: Press (Stop Rain + Stop Snow)")
+    let c= alert("To Start Rain:   Press (Start Rain) \nTo Stop Rain:   Press (Stop Rain) \nTo Start Snow:   Press (Start Snow) \nTo Stop Snow:   Press (Stop Snow) \nTo Start Rain + Snow:   Press (Start Rain + Start Snow) \nTo Stop Rain + Snow:   Press (Stop Rain + Stop Snow)")
 }
