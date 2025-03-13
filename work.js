1bf67f41943b8bcf437412be17b754b8ba043b64
Faizanhaider461 <syedfaizan768768@gmail.com>
2025-03-14 02:28:19 +0500

    Add files via upload

---
+++ work.js
@@ -0,0 +1,18 @@
+let a= document.querySelector(".img")
+
+
+
+
+function startRain() {
+    
+    a.style.display = "block"
+}
+
+function stopRain() {
+
+    a.style.display = "none"
+}
+
+function showpopup(){
+    let b= alert("Thanks for watching this animation")
+}
